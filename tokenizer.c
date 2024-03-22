21914e2f39324fda7997dcaab938117c18f3d5f9
ddrie <94722943+ddrie@users.noreply.github.com>
2024-03-22 16:38:42 +0100

    small update

--- tokenizer.c
+++ tokenizer.c
@@ -26,42 +26,61 @@ int count_tokens(const char *input)
     return tokens;
 }
 
-char **tokenizer(const char *input) {
-    bool in_single_quote = false;
-    bool in_double_quote = false;
-    int token_count = count_tokens(input); 
+bool update_token_state(char c, bool *in_single_quote, bool *in_double_quote) 
+{
+    if (c == '\'' && !*in_double_quote) 
+    {
+        *in_single_quote = !*in_single_quote;
+        return true;
+    } 
+    else if (c == '\"' && !*in_single_quote) 
+    {
+        *in_double_quote = !*in_double_quote;
+        return true; 
+    }
+    return false;
+}
+
+bool add_token(const char *input, int *start_index, int end_index, char ***tokens, int *token_count) 
+{
+    int token_len = end_index - *start_index + 1;
+    int token_len = input[i + 1] == '\0' && input[i] != ' ' ? token_len + 1 : token_len;
+
+    char *token = malloc(token_len + 1);
+    if (!token) 
+    {
+        return false;
+    }
+
+    strncpy(token, &input[*start_index], token_len);
+    token[token_len] = '\0';
+    (*tokens)[(*token_count)++] = token;
+    *start_index = end_index + 1;
+    return true;
+}
+
+char **tokenizer(const char *input) 
+{
+    bool in_single_quote = false, in_double_quote = false;
+    int token_count = count_tokens(input);
     char **tokens = calloc(token_count + 1, sizeof(char *));
+    int i = 0;
+    int j = 0;
+    int start_index = 0
+    int current_token_count = 0;
     if (!tokens) return NULL;
 
-    const char *token_start = input;
-    int i = 0, current_token_index = 0;
-    int j = 0;
-
-    while (input[i] != '\0') 
+    
+    while (input[i] != '\0')
     {
-        bool at_end_of_token = false;
-
-        if (input[i] == '\'' && !in_double_quote) 
+        if (update_token_state(input[i], &in_single_quote, &in_double_quote)) continue;
+        
+        if ((input[i] == ' ' && !(in_single_quote || in_double_quote)) || input[i+1] == '\0') 
         {
-            in_single_quote = !in_single_quote;
-            i++;
-            continue;
-        } 
-        else if (input[i] == '\"' && !in_single_quote) 
-        {
-            in_double_quote = !in_double_quote;
-            i++;
-            continue;
-        }
-
-        if ((input[i] == ' ' && !in_single_quote && !in_double_quote) || (input[i + 1] == '\0' && !(input[i] == ' ' && !in_single_quote && !in_double_quote))) 
-        {
-            at_end_of_token = true;
-            int token_len = (i - (token_start - input)) + (input[i + 1] == '\0' ? 1 : 0); 
-            char *token = malloc(token_len + 1);
-            if (!token) 
+            if (input[i+1] == '\0' && !(input[i] == ' ')) i++;
+            if (!add_token(input, &start_index, i, &tokens, &current_token_count)) 
             {
-                while(j < current_token_index)
+                while(j < current_token_count)
                 {
                     free(tokens[j]);
                     j++;
@@ -69,15 +88,11 @@ char **tokenizer(const char *input) {
                 free(tokens);
                 return NULL;
             }
-            strncpy(token, token_start, token_len);
-            token[token_len] = '\0'; 
-            tokens[current_token_index++] = token; 
-            token_start = input + i + 1;
         }
         i++;
     }
 
-    tokens[current_token_index] = NULL;
+    tokens[current_token_count] = NULL;
     return tokens;
 }
 
